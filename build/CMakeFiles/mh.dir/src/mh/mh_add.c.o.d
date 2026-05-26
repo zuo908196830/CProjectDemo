fbed518303978e93e95177c46430bf422d20e63e
zuojianxiong <zuojianxiong@douban.com>
2026-05-26 18:10:03 +0800

    init

--- build/CMakeFiles/mh.dir/src/mh/mh_add.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/mh.dir/src/mh/mh_add.c.o: \
-  /Users/zzz/cproject/demo/src/mh/mh_add.c \
-  /Users/zzz/cproject/demo/include/mh/mh_add.h \
-  /Users/zzz/cproject/demo/include/test_fun/t_add.h
--- build/CMakeFiles/test_fun.dir/src/test_fun/t_add.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_fun.dir/src/test_fun/t_add.c.o: \
-  /Users/zzz/cproject/demo/src/test_fun/t_add.c \
-  /Users/zzz/cproject/demo/include/test_fun/t_add.h
